a182c5fe3167340a66078c4469a9c785f8f1a7ae
vansh9324 <115867897+vansh9324@users.noreply.github.com>
2025-06-23 17:36:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ with right:
                 alt.Tooltip("Registrations:Q", title="Registrations"),
                 alt.Tooltip("Top Karyakarta:N", title="Top Karyakarta")
             ])
-        .properties(height=500))
+        .properties(height=350))
     st.altair_chart(ks_chart, use_container_width=True)
 
 st.markdown("---")
